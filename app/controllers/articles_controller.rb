dd1515eb9b41460199f27780eea65183f11bc05a
RuiFerreira <ruip.b.a.ferreira@gmail.com>
2022-07-04 14:08:58 +0100

    Articles can now be edited

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,6 +24,20 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = 'Article successfully edited'
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   # gets article params from update and create form submitions

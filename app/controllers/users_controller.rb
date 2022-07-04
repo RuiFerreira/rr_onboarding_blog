0c0c2554c12d0b82c53f38bd17f8608a0ce221c4
RuiFerreira <ruip.b.a.ferreira@gmail.com>
2022-07-04 15:07:43 +0100

    Changes to form_errors component to support user creation and edition forms. Users can now sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome #{@user.username}, you are now a member of Runtime Revolution's Blog!"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email)
+  end
 end

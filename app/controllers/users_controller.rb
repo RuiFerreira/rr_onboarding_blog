930649b663d92064738c22917105c325d873de14
RuiFerreira <ruip.b.a.ferreira@gmail.com>
2022-07-06 12:23:34 +0100

    Added password to user sign up and edition forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,7 +49,7 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:username, :email)
+    params.require(:user).permit(:username, :email, :password)
   end
 
 end

396cefa7d9fe6fa1f3dc0cc05a66553a2f3d1805
RuiFerreira <ruip.b.a.ferreira@gmail.com>
2022-07-06 14:01:32 +0100

    Users can now log in and out of the platform

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   resources :users, except: [:new]
   get 'signup', to: 'users#new' # this will force the route shown to the user to be /signup instead of /user/new
   get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end

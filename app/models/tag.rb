3b116c876905e5726df0f86630f1938b4a02f3ea
RuiFerreira <ruip.b.a.ferreira@gmail.com>
2022-07-07 11:10:41 +0100

    Added presence, uniqueness and length validations to tag model

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,5 @@
 class Tag < ApplicationRecord
+  validates :name, presence: true,
+                   uniqueness: [case_sensitive: false],
+                   length: { minimum: 3, maximum: 20 }
 end

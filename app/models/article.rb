97c58aac6773911f812b9269d65be216f1bf5315
RuiFerreira <ruip.b.a.ferreira@gmail.com>
2022-07-13 11:46:52 +0100

    small changes

--- app/models/article.rb
+++ app/models/article.rb
@@ -24,7 +24,7 @@ class Article < ApplicationRecord
 
   scope :filter_by_tags, ->(tag_names, article_list) {
     # article_list.includes(article_tags: :tag).where(article_tags: { tags: { name: tag_names } })
-    article_list.joins(:tagged_on)
+    article_list
   }
 
 end
